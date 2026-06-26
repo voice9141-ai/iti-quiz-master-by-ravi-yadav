4738d72b4ee046426e3b3a829376f58936b6bd11
voice9141-ai <voice9141@gmail.com>
2026-06-27 03:30:25 +0530

    Add quiz question and answer checking functionality

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
+const question = {
+  question: "1. Which device converts AC to DC?",
+  A: "Transformer",
+  B: "Rectifier",
+  C: "Capacitor",
+  D: "Inductor",
+  answer: "B"
+};
 
 function startQuiz() {
-    alert("Welcome to Ravi Tech Academy 🚀");
+  document.getElementById("quiz").style.display = "block";
+
+  document.getElementById("question").innerText = question.question;
+  document.getElementById("A").innerText = question.A;
+  document.getElementById("B").innerText = question.B;
+  document.getElementById("C").innerText = question.C;
+  document.getElementById("D").innerText = question.D;
+}
+
+function checkAnswer(option) {
+  if (option === question.answer) {
+    alert("✅ Correct Answer!");
+  } else {
+    alert("❌ Wrong Answer!");
+  }
 }
