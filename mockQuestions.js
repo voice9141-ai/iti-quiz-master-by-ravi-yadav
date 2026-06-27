ca62eef287d607a5c0369c30a280342dce610af1
Baban <voice9141@gmail.com>
2026-06-27 16:49:43 +0530

    Update mockQuestions.js

--- mockQuestions.js
+++ mockQuestions.js
@@ -1,1 +1,26 @@
-
+const mockQuestions = [
+  {
+    question: "1. What is the shape of mandatory signs?",
+    A: "Square",
+    B: "Circular",
+    C: "Triangular",
+    D: "Rectangular",
+    answer: "B"
+  },
+  {
+    question: "2. Which type of toe caps is used to avoid crushing of feet while shifting equipments?",
+    A: "Steel toe caps",
+    B: "Plastic toe caps",
+    C: "Rubber toe caps",
+    D: "Leather toe caps",
+    answer: "A"
+  },
+  {
+    question: "3. What is the treatment provided for a person suffering from electric shock?",
+    A: "Provide water",
+    B: "Keep the victim cold",
+    C: "Cover the victim with a coat",
+    D: "Move the victim to a ventilated place",
+    answer: "D"
+  }
+];
