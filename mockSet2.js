0c237429b24cb539e6ac9fc0effbac4787803238
Baban <voice9141@gmail.com>
2026-06-28 00:55:03 +0530

    Update mockSet2.js

--- mockSet2.js
+++ mockSet2.js
@@ -1,1 +1,601 @@
-const mockQuestionsSet2 = [];
+const mockQuestionsSet2 = [
+{
+  question: "1. Which terminal of the meter is connected for measuring electrical quantity?",
+  A: "Input terminal",
+  B: "Output terminal",
+  C: "Meter movement",
+  D: "Pointer mechanism",
+  answer: "A"
+},
+{
+  question: "2. How electrical quantity is measured by the meter marked on it?",
+  A: "Using colour codes",
+  B: "Printing the values",
+  C: "Using parameter symbols",
+  D: "Directly printing the specifications",
+  answer: "C"
+},
+{
+  question: "3. Which electrical parameter is measured by megger?",
+  A: "Current",
+  B: "Voltage",
+  C: "Insulation resistance",
+  D: "Frequency",
+  answer: "C"
+},
+{
+  question: "4. What are insulators called?",
+  A: "Thyristors",
+  B: "Molecules",
+  C: "Dielectric",
+  D: "Semiconductors",
+  answer: "C"
+},
+{
+  question: "5. What is the value of each division marked by numbers on the voltmeter shown in the figure?",
+  A: "1 V",
+  B: "2 V",
+  C: "4 V",
+  D: "5 V",
+  answer: "B"
+},
+{
+  question: "6. What is the ideal AC voltage in domestic electricity?",
+  A: "440 V",
+  B: "240 V",
+  C: "150 V",
+  D: "110 V",
+  answer: "B"
+},
+{
+  question: "7. What symbol on the meter indicates the measurement of voltage for both AC and DC?",
+  A: "Symbol 1",
+  B: "Symbol 2",
+  C: "AC/DC Voltage Symbol",
+  D: "Symbol 4",
+  answer: "C"
+},
+{
+  question: "8. What is the degree for AC signal to complete one cycle?",
+  A: "90°",
+  B: "180°",
+  C: "270°",
+  D: "360°",
+  answer: "D"
+},
+{
+  question: "9. What is the purpose of test lamp?",
+  A: "To measure voltage",
+  B: "To measure current",
+  C: "To identify phase and current",
+  D: "To measure power",
+  answer: "C"
+},
+{
+  question: "10. What is the name of the voltage measured between any two lines in a three phase AC circuit?",
+  A: "Phase voltage",
+  B: "Line voltage",
+  C: "Supply voltage",
+  D: "Load voltage",
+  answer: "B"
+},
+{
+  question: "11. Which electric measuring instrument is used for measuring potential difference?",
+  A: "Volt meter",
+  B: "Ammeter",
+  C: "Galvanometer",
+  D: "Ohm meter",
+  answer: "A"
+},
+{
+  question: "12. Which electrical measuring instrument is used to measure current?",
+  A: "Volt meter",
+  B: "Ammeter",
+  C: "Ohm meter",
+  D: "Watt meter",
+  answer: "B"
+},
+{
+  question: "13. How ammeter is connected for the measurement of current in an electrical circuit?",
+  A: "Across the source",
+  B: "Across the load",
+  C: "In parallel with fuse",
+  D: "In series with load",
+  answer: "D"
+},
+{
+  question: "14. What is the symbol for moving iron type measuring instrument?",
+  A: "Symbol 1",
+  B: "Symbol 2",
+  C: "Symbol 3",
+  D: "Moving Iron Symbol",
+  answer: "D"
+},
+{
+  question: "15. What is the other name for tong tester?",
+  A: "Clamp meter",
+  B: "Multimeter",
+  C: "Volt meter",
+  D: "Ammeter",
+  answer: "A"
+},
+{
+  question: "16. Which is a contact less meter?",
+  A: "Ammeter",
+  B: "Volt meter",
+  C: "Multimeter",
+  D: "Clamp meter",
+  answer: "D"
+},
+{
+  question: "17. Which current is used in clamp meter?",
+  A: "Large alternating current",
+  B: "Small alternating current",
+  C: "Small direct current",
+  D: "Large direct current",
+  answer: "A"
+},
+{
+  question: "18. What is the name of the pair of metal strips used in battery cell?",
+  A: "Cathode",
+  B: "Electrolyte",
+  C: "Electrodes",
+  D: "Carbon rod",
+  answer: "C"
+},
+{
+  question: "19. Which electrolyte is used in lead-acid battery?",
+  A: "Zinc chloride",
+  B: "Sulphuric acid",
+  C: "Alkaline solution",
+  D: "Potassium hydroxide solution",
+  answer: "B"
+},
+{
+  question: "20. How batteries are classified?",
+  A: "Dry cells and alkaline cells",
+  B: "Button cells and lithium cells",
+  C: "Primary cells and secondary cells",
+  D: "Cylindrical cells and rectangular cells",
+  answer: "C"
+},
+{
+  question: "21. What is the colour of positive electrode in a fully charged lead-acid battery?",
+  A: "Red",
+  B: "Grey",
+  C: "Reddish brown",
+  D: "Spongy grey",
+  answer: "C"
+},
+{
+  question: "22. Which method is adopted to charge a car battery with voltage rating of 2.3 V per cell?",
+  A: "Float charging method",
+  B: "Trickle charging method",
+  C: "Constant current charging method",
+  D: "Constant voltage charging method",
+  answer: "D"
+},
+{
+  question: "23. What is the specific gravity of concentrated sulphuric acid?",
+  A: "1.175",
+  B: "1.245",
+  C: "1.835",
+  D: "1.945",
+  answer: "C"
+},
+{
+  question: "24. Which electrolyte is used in the maintenance free lead acid batteries?",
+  A: "Gelled electrolyte",
+  B: "Sodium electrolyte",
+  C: "Ceramic electrolyte",
+  D: "Potassium electrolyte",
+  answer: "A"
+},
+{
+  question: "25. What is the rated voltage of a single cell in lead-acid battery?",
+  A: "1.5 V",
+  B: "2.0 V",
+  C: "2.2 V",
+  D: "12 V",
+  answer: "C"
+},
+{
+  question: "26. What is the effect on a secondary cell supplying current to the load?",
+  A: "Leaking",
+  B: "Charging",
+  C: "Unloading",
+  D: "Discharging",
+  answer: "D"
+},
+{
+  question: "27. What is the total voltage of six 1.5 V cells connected in series?",
+  A: "3 VDC",
+  B: "6 VDC",
+  C: "9 VDC",
+  D: "12 VDC",
+  answer: "C"
+},
+{
+  question: "28. Which type of connection increases the capacity rating of two or more batteries?",
+  A: "Parallel",
+  B: "Series",
+  C: "Series-Parallel",
+  D: "Opposite",
+  answer: "A"
+},
+{
+  question: "29. What is the full form of PMMC meter?",
+  A: "Parallel Magnet Moving Coil Meter",
+  B: "Position Magnet Moving Coil Meter",
+  C: "Principle Magnet Moving Coil Meter",
+  D: "Permanent Magnet Moving Coil Meter",
+  answer: "D"
+},
+{
+  question: "30. Which bearing supports the shaft of a moving coil assembly in a PMMC instrument?",
+  A: "Steel bearings",
+  B: "Bush bearings",
+  C: "Jeweled bearings",
+  D: "Gun metal bearings",
+  answer: "C"
+},
+{
+  question: "31. Which characteristic enables the deflection of pointer in the attraction type moving iron meter?",
+  A: "Weight of the soft iron pieces",
+  B: "Deflection is inversely proportional to current",
+  C: "Deflection is independent of current direction",
+  D: "Deflecting and controlling torques are unequal",
+  answer: "C"
+},
+{
+  question: "32. Which parameter is measured by a multimeter?",
+  A: "Voltage",
+  B: "Energy",
+  C: "Frequency",
+  D: "Time duration",
+  answer: "A"
+},
+{
+  question: "33. What is the value of shunt resistance required for 1 mA meter to extend the range and measure 10 mA (Rm = 27 Ω)?",
+  A: "1 Ω",
+  B: "2 Ω",
+  C: "3 Ω",
+  D: "4 Ω",
+  answer: "C"
+},
+{
+  question: "34. What is the advantage of using digital multimeter?",
+  A: "Accuracy",
+  B: "Linear scale",
+  C: "Easy portability",
+  D: "Logarithmic scale",
+  answer: "A"
+},
+{
+  question: "35. What is the name of the symbol marked as 'X' in the panel meter shown in the figure?",
+  A: "Moving coil meter",
+  B: "Moving iron meter",
+  C: "Hot wire instrument",
+  D: "Moving coil with rectifier",
+  answer: "B"
+},
+{
+  question: "36. What control is used in repulsion type moving iron instruments to maintain the pointer at zero position?",
+  A: "Spring control",
+  B: "Air damping control",
+  C: "Magnetic repulsion control",
+  D: "Magnetic attraction control",
+  answer: "A"
+},
+{
+  question: "37. Which torque is used in PMMC meter movement?",
+  A: "Low torque",
+  B: "High torque",
+  C: "Moderate torque",
+  D: "Insufficient torque",
+  answer: "C"
+},
+{
+  question: "38. Which meter uses a moving coil for measurement?",
+  A: "LCR meter",
+  B: "PMMC meter",
+  C: "MI repulsion type",
+  D: "MI attraction type",
+  answer: "B"
+},
+{
+  question: "39. Which one is non-metal?",
+  A: "Mercury",
+  B: "Graphite",
+  C: "Brass",
+  D: "Iron",
+  answer: "B"
+},
+{
+  question: "40. Which metal contains iron as a major content?",
+  A: "Brass metal",
+  B: "Bronze metal",
+  C: "Zinc",
+  D: "Ferrous metal",
+  answer: "D"
+},
+{
+  question: "41. What is termed as the quantity of matter contained in a body?",
+  A: "Density",
+  B: "Volume",
+  C: "Mass",
+  D: "Specific gravity",
+  answer: "C"
+},
+{
+  question: "42. What is called mass per unit volume of a substance?",
+  A: "Mass",
+  B: "Weight",
+  C: "Density",
+  D: "Volume",
+  answer: "C"
+},
+{
+  question: "43. What is the SI unit of heat?",
+  A: "Calorie",
+  B: "Joule",
+  C: "Centigrade heat unit",
+  D: "British thermal unit",
+  answer: "B"
+},
+{
+  question: "44. What is the value of specific heat of water?",
+  A: "4",
+  B: "3",
+  C: "2",
+  D: "1",
+  answer: "D"
+},
+{
+  question: "45. Identify the conventional symbol of material.",
+  A: "Lead",
+  B: "Steel",
+  C: "Glass",
+  D: "Porcelain",
+  answer: "B"
+},
+{
+  question: "46. Identify the conventional symbol of material.",
+  A: "Concrete",
+  B: "Steel",
+  C: "Wood",
+  D: "Glass",
+  answer: "D"
+},
+{
+  question: "47. Identify the conventional symbol of material.",
+  A: "Steel",
+  B: "Wood",
+  C: "Glass",
+  D: "Concrete",
+  answer: "D"
+},
+{
+  question: "48. What is the trimmed size of A4 sheet?",
+  A: "841 × 1189",
+  B: "594 × 841",
+  C: "210 × 297",
+  D: "148 × 210",
+  answer: "C"
+},
+{
+  question: "49. Which drawing dimension is correct as per IS standard?",
+  A: "Option shown in Figure A",
+  B: "Option shown in Figure B",
+  C: "Option shown in Figure C",
+  D: "Option shown in Figure D",
+  answer: "A"
+},
+{
+  question: "50. Identify the name of prism?",
+  A: "Square prism",
+  B: "Pentagonal prism",
+  C: "Triangular prism",
+  D: "Hexagonal prism",
+  answer: "D"
+},
+{
+  question: "51. Fill in the blank with correct word. 'They _________ good friends.'",
+  A: "is",
+  B: "am",
+  C: "are",
+  D: "was",
+  answer: "C"
+},
+{
+  question: "52. Fill in the blank with present progressive tense of the verb. 'The train __________ through the tunnel.'",
+  A: "passed",
+  B: "is passing",
+  C: "had passed",
+  D: "was passing",
+  answer: "B"
+},
+{
+  question: "53. Fill in the blank with proper interrogative adjective. '___________ are you going?'",
+  A: "Who",
+  B: "Where",
+  C: "Which",
+  D: "What",
+  answer: "B"
+},
+{
+  question: "54. Fill in the blank with proper pronoun. 'I made this cake ___________.'",
+  A: "myself",
+  B: "yourself",
+  C: "himself",
+  D: "itself",
+  answer: "A"
+},
+{
+  question: "55. Fill in the blank with proper pronoun. 'She made this cake ___________.'",
+  A: "itself",
+  B: "myself",
+  C: "herself",
+  D: "himself",
+  answer: "C"
+},
+{
+  question: "56. Fill in the blank with proper reflexive pronoun. 'She has hurt ___________.'",
+  A: "myself",
+  B: "herself",
+  C: "himself",
+  D: "itself",
+  answer: "B"
+},
+{
+  question: "57. Fill in the blank with suitable adjective for the sentence. 'The little girl's ______ eyes revealed her mischief.'",
+  A: "chubby",
+  B: "weak",
+  C: "short sighted",
+  D: "twinkling",
+  answer: "D"
+},
+{
+  question: "58. Fill in the blank with suitable place preposition. 'My house is __________ the third floor.'",
+  A: "at",
+  B: "in",
+  C: "on",
+  D: "under",
+  answer: "C"
+},
+{
+  question: "59. Fill in the correct question word. '___________ is the speaker at the function?'",
+  A: "What",
+  B: "Who",
+  C: "Where",
+  D: "When",
+  answer: "B"
+},
+{
+  question: "60. Which one is the major change that takes place in manufacturing?",
+  A: "Green revolution",
+  B: "White revolution",
+  C: "Industrial revolution",
+  D: "Blue revolution",
+  answer: "C"
+},
+{
+  question: "61. Which one is NOT a noun?",
+  A: "Honesty",
+  B: "Teacher",
+  C: "School",
+  D: "Quickly",
+  answer: "D"
+},
+{
+  question: "62. Which word is the opposite of 'Victory'?",
+  A: "Success",
+  B: "Defeat",
+  C: "Achievement",
+  D: "Triumph",
+  answer: "B"
+},
+{
+  question: "63. Which one is NOT a speech sound?",
+  A: "Vowel",
+  B: "Intonation",
+  C: "Consonant",
+  D: "Pronunciation",
+  answer: "B"
+},
+{
+  question: "64. The word that expresses a sudden and strong feeling is called _________.",
+  A: "Punctuation",
+  B: "Interjection",
+  C: "Conjunction",
+  D: "Apostrophe",
+  answer: "B"
+},
+{
+  question: "65. When you greet higher officials such as Teacher, Instructor or Supervisor, you should use _________.",
+  A: "Good morning",
+  B: "Hello",
+  C: "Hey",
+  D: "Hi",
+  answer: "A"
+},
+{
+  question: "66. Which is a silent letter in the word 'ANSWER'?",
+  A: "R",
+  B: "E",
+  C: "S",
+  D: "W",
+  answer: "D"
+},
+{
+  question: "67. Which one is a 'Do' of discussion etiquette?",
+  A: "Lose your temper",
+  B: "Listen to others",
+  C: "Talk about irrelevant details",
+  D: "Interrupt others",
+  answer: "B"
+},
+{
+  question: "68. Which one is a 'Don't' of discussion etiquette?",
+  A: "Be open minded",
+  B: "Use moderate tone",
+  C: "Listen to others",
+  D: "Argue unnecessary",
+  answer: "D"
+},
+{
+  question: "69. Which one is a brain of computer?",
+  A: "Keyboard",
+  B: "CPU",
+  C: "Monitor",
+  D: "Hard disk",
+  answer: "B"
+},
+{
+  question: "70. Which one is an exclamatory sentence?",
+  A: "What a beautiful house it is!",
+  B: "It is a beautiful house.",
+  C: "Is it a beautiful house?",
+  D: "Your house is beautiful.",
+  answer: "A"
+},
+{
+  question: "71. Which one is in active voice?",
+  A: "Ram has passed the exam.",
+  B: "The ball was caught by him.",
+  C: "The book was being read by her.",
+  D: "We were driven home by dad.",
+  answer: "A"
+},
+{
+  question: "72. Which one is in passive voice?",
+  A: "Mohan is painting a house.",
+  B: "She was reading a book.",
+  C: "Her birthday was celebrated by us.",
+  D: "I have seen that movie.",
+  answer: "C"
+},
+{
+  question: "73. Which one is NOT a conjunction?",
+  A: "And",
+  B: "Or",
+  C: "But",
+  D: "On",
+  answer: "D"
+},
+{
+  question: "74. Which one is NOT a benefit of the role playing?",
+  A: "Builds confidence",
+  B: "Develops listening skill",
+  C: "Develops creative problem-solving skill",
+  D: "Develops boredom",
+  answer: "D"
+},
+{
+  question: "75. Which one is NOT a vowel?",
+  A: "a",
+  B: "e",
+  C: "f",
+  D: "i",
+  answer: "C"
+}
