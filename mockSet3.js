2e082a8a265256b4b107da89bab10641f19611ef
Baban <voice9141@gmail.com>
2026-06-28 01:17:35 +0530

    Update mockSet3.js

--- mockSet3.js
+++ mockSet3.js
@@ -1,1 +1,601 @@
-const mockQuestionsSet3 = [];
+const mockSet3Questions = [
+{
+  question: "1. Which electrode controls the brightness of the image on the screen of oscilloscope?",
+  A: "Anode",
+  B: "Cathode",
+  C: "Control grid",
+  D: "Focussing electrode",
+  answer: "C"
+},
+{
+  question: "2. Which instrument is used to measure resistance, capacitance and inductance?",
+  A: "LCR bridge",
+  B: "Wein bridge",
+  C: "Kelvin bridge",
+  D: "Wheatstone bridge",
+  answer: "A"
+},
+{
+  question: "3. Which band is used for UHF in International Telecommunication System?",
+  A: "Band 4",
+  B: "Band 6",
+  C: "Band 9",
+  D: "Band 11",
+  answer: "C"
+},
+{
+  question: "4. Which band of frequency is used for RADAR in frequency spectrum allotted by the International Telecommunication Union (ITU)?",
+  A: "ITU Band-4",
+  B: "ITU Band-6",
+  C: "ITU Band-8",
+  D: "ITU Band-10",
+  answer: "D"
+},
+{
+  question: "5. Which type of wave is generated in Schmitt trigger circuit?",
+  A: "Sine wave",
+  B: "Square wave",
+  C: "Saw tooth wave",
+  D: "Triangular wave",
+  answer: "B"
+},
+{
+  question: "6. Which parameter is used in the working of moving coil meter?",
+  A: "Spring control",
+  B: "Stray magnetic fields",
+  C: "Eddy current damping",
+  D: "Permanent magnetic fields",
+  answer: "D"
+},
+{
+  question: "7. Which arrangement has the high value of resistor connected to extend the range of voltmeter?",
+  A: "Star",
+  B: "Delta",
+  C: "Series",
+  D: "Parallel",
+  answer: "C"
+},
+{
+  question: "8. Which analog meter has a battery provided?",
+  A: "Ammeter",
+  B: "Voltmeter",
+  C: "Ohm meter",
+  D: "Watt meter",
+  answer: "C"
+},
+{
+  question: "9. Which meter movement is not affected by stray magnetic fields?",
+  A: "PMMC meter",
+  B: "Thermocouple meter",
+  C: "MI meter - Attraction type",
+  D: "MI meter - Repulsion type",
+  answer: "A"
+},
+{
+  question: "10. Which measuring instrument has the movement shown in the figure?",
+  A: "Moving coil instrument",
+  B: "Attraction type MI meter",
+  C: "Repulsion type MI meter",
+  D: "Centre zero galvanometer",
+  answer: "A"
+},
+{
+  question: "11. Which position is the moving coil meter kept for measurements shown in the figure?",
+  A: "Vertical",
+  B: "Horizontal",
+  C: "Inclined by 45°",
+  D: "Inclined by 60°",
+  answer: "A"
+},
+{
+  question: "12. What is the full form of CRT?",
+  A: "Current Ray Tube",
+  B: "Clear Ray Tube",
+  C: "Cathode Ray Tube",
+  D: "Carbon Ray Tube",
+  answer: "C"
+},
+{
+  question: "13. Which function control in CRO adjusts the trace sharper?",
+  A: "Focus",
+  B: "Intensity",
+  C: "Time/Div Trigger",
+  D: "Amplitude (V/Div)",
+  answer: "A"
+},
+{
+  question: "14. Which meter is used for the measurement of Voltage, Current and Resistance?",
+  A: "Voltmeter",
+  B: "Ammeter",
+  C: "Wattmeter",
+  D: "Multimeter",
+  answer: "D"
+},
+{
+  question: "15. What is the maximum current measurement possible with digital multimeter?",
+  A: "10 mA",
+  B: "100 mA",
+  C: "10 A",
+  D: "100 A",
+  answer: "C"
+},
+{
+  question: "16. Which control knob changes the vertical shape of the waveform?",
+  A: "Time/Div",
+  B: "Volt/Div",
+  C: "Hold",
+  D: "Trigger",
+  answer: "B"
+},
+{
+  question: "17. Which control knob changes the horizontal shape of the waveform?",
+  A: "Volt/Div",
+  B: "Time/Div",
+  C: "Hold",
+  D: "Trigger",
+  answer: "B"
+},
+{
+  question: "18. Which electrode changes the fluorescent screen brightness of CRO?",
+  A: "Cathode",
+  B: "Anode",
+  C: "Control grid",
+  D: "Screen grid",
+  answer: "C"
+},
+{
+  question: "19. What is used to measure the values of resistance, inductance and capacitance?",
+  A: "Ohm meter",
+  B: "Multimeter",
+  C: "Potentiometer",
+  D: "LCR meter",
+  answer: "D"
+},
+{
+  question: "20. What is the function of signal generator?",
+  A: "To generate current",
+  B: "To generate voltage",
+  C: "To generate various shape waveform signals",
+  D: "To display the signal",
+  answer: "C"
+},
+{
+  question: "21. Which is the main part of signal generator?",
+  A: "Oscillator",
+  B: "Amplifier",
+  C: "Rectifier",
+  D: "Regulator",
+  answer: "A"
+},
+{
+  question: "22. What is the range of audio frequency in human beings?",
+  A: "20 MHz to 200 MHz",
+  B: "2 MHz to 20 MHz",
+  C: "20 kHz to 200 kHz",
+  D: "20 Hz to 20 kHz",
+  answer: "D"
+},
+{
+  question: "23. Which button is used to change the shape of waveform in signal generator?",
+  A: "Amplitude",
+  B: "Frequency",
+  C: "Function",
+  D: "Attenuator",
+  answer: "C"
+},
+{
+  question: "24. What is the basic difference between a square wave and pulse wave?",
+  A: "Waveform shape",
+  B: "Duty cycles",
+  C: "Frequency range",
+  D: "Amplitude",
+  answer: "B"
+},
+{
+  question: "25. What is the frequency range of VHF?",
+  A: "3×10⁶ to 3×10⁷ Hz",
+  B: "3×10⁷ to 3×10⁸ Hz",
+  C: "3×10⁸ to 3×10⁹ Hz",
+  D: "3×10⁹ to 3×10¹⁰ Hz",
+  answer: "B"
+},
+{
+  question: "26. What is the full form of D.S.O?",
+  A: "Dual System Oscillator",
+  B: "Dual Storage Oscillator",
+  C: "Digital System Oscilloscope",
+  D: "Digital Storage Oscilloscope",
+  answer: "D"
+},
+{
+  question: "27. Which acquisition mode is used by the DSO to sample the highest and lowest values of the input signal?",
+  A: "Auto mode",
+  B: "Sample mode",
+  C: "Average mode",
+  D: "Peak detect mode",
+  answer: "D"
+},
+{
+  question: "28. How the overall operation of DSO is controlled?",
+  A: "Using microprocessors",
+  B: "Using ICs and transistors",
+  C: "Using discrete components",
+  D: "Using diodes and transistors",
+  answer: "A"
+},
+{
+  question: "29. What is the advantage of Digital Storage Oscilloscope?",
+  A: "Process signals in analog format",
+  B: "Makes measurement of digital data",
+  C: "Stores digital data for later viewing",
+  D: "Electron beam moves across the screen",
+  answer: "C"
+},
+{
+  question: "30. What is the name of the circuit shown in the figure?",
+  A: "Astable multivibrator",
+  B: "Bistable multivibrator",
+  C: "RC coupled amplifier",
+  D: "Monostable multivibrator",
+  answer: "A"
+},
+{
+  question: "31. What type of waveform is available at pin number 2 of function generator IC 8038?",
+  A: "Sine wave",
+  B: "Square wave",
+  C: "Triangular wave",
+  D: "Modulated wave",
+  answer: "A"
+},
+{
+  question: "32. What is the name of the circuit built with IC 8038 shown in the figure?",
+  A: "Pulse generator",
+  B: "Sine wave generator",
+  C: "Square wave generator",
+  D: "Function generator",
+  answer: "D"
+},
+{
+  question: "33. Which type of waveform is available at pin number 3 of IC 8038 function generator?",
+  A: "Sine wave",
+  B: "Square wave",
+  C: "Triangle wave",
+  D: "Modulated wave",
+  answer: "C"
+},
+{
+  question: "34. Which function is performed by the sample / Hold circuit along with the ADC in Digital Storage Oscilloscope?",
+  A: "Storage",
+  B: "Data display",
+  C: "Data acquisition",
+  D: "Upload to computer",
+  answer: "C"
+},
+{
+  question: "35. What is the name of the waveform displayed on the DSO screen shown in the figure?",
+  A: "DC waveform",
+  B: "Pulse waveform",
+  C: "Ringing waveform",
+  D: "Saw tooth waveform",
+  answer: "C"
+},
+{
+  question: "36. How digital equipment works with the input voltage samples?",
+  A: "Constant output voltage",
+  B: "Continuously variable voltage",
+  C: "Continuously variable current",
+  D: "Converts it to Binary numbers",
+  answer: "D"
+},
+{
+  question: "37. What is the name of the factory setup done to the Digital Storage Oscilloscope?",
+  A: "Normal setup",
+  B: "Factory setup",
+  C: "Default setup",
+  D: "Measurement setup",
+  answer: "C"
+},
+{
+  question: "38. Which part of the DSO stores the processed data of input signal voltage?",
+  A: "Memory",
+  B: "Screen display",
+  C: "Analog to digital converter",
+  D: "Digital to analog converter",
+  answer: "A"
+},
+{
+  question: "39. What is the boiling point of water?",
+  A: "0°C",
+  B: "32°C",
+  C: "100°C",
+  D: "212°C",
+  answer: "C"
+},
+{
+  question: "40. Which is the unit of current?",
+  A: "Ampere",
+  B: "Volt",
+  C: "Ohm",
+  D: "Watt",
+  answer: "A"
+},
+{
+  question: "41. What is the flow of electrons in any conductor?",
+  A: "Voltage",
+  B: "Current",
+  C: "Resistance",
+  D: "Power",
+  answer: "B"
+},
+{
+  question: "42. Which is very good conductor?",
+  A: "Copper",
+  B: "Cast iron",
+  C: "Wrought iron",
+  D: "Steel",
+  answer: "A"
+},
+{
+  question: "43. Which is mineral insulator?",
+  A: "Glass",
+  B: "Quartz",
+  C: "Mica",
+  D: "Porcelain",
+  answer: "C"
+},
+{
+  question: "44. Which is the unit of electrical power?",
+  A: "Volts",
+  B: "Ohms",
+  C: "Watts",
+  D: "Ampere",
+  answer: "C"
+},
+{
+  question: "45. Identify the name of tool?",
+  A: "Try square",
+  B: "Surface gauge",
+  C: "Steel rule",
+  D: "Feeler gauge",
+  answer: "A"
+},
+{
+  question: "46. Identify the name of tool?",
+  A: "Ball pein hammer",
+  B: "Cross pein hammer",
+  C: "Straight pein hammer",
+  D: "Sledge hammer",
+  answer: "A"
+},
+{
+  question: "47. Identify the name of tool?",
+  A: "Hawk-billed shear",
+  B: "Aviation shear",
+  C: "Cutting plier",
+  D: "Double cutting shear",
+  answer: "C"
+},
+{
+  question: "48. Identify the name of tool?",
+  A: "Ball pein hammer",
+  B: "Mallet",
+  C: "Cross pein hammer",
+  D: "Straight pein hammer",
+  answer: "B"
+},
+{
+  question: "49. Identify the name of tool?",
+  A: "Open end spanner",
+  B: "Ring spanner",
+  C: "Combination spanner",
+  D: "Adjustable spanner",
+  answer: "A"
+},
+{
+  question: "50. Which dimension correctly marked in the circle as per standard?",
+  A: "Figure A",
+  B: "Figure B",
+  C: "Figure C",
+  D: "Figure D",
+  answer: "B"
+},
+{
+  question: "51. Which one of the following is a good office etiquette?",
+  A: "One should dress formally",
+  B: "One should not be punctual to work",
+  C: "One should have fancy mobile ring tone",
+  D: "One should litter one's work place",
+  answer: "A"
+},
+{
+  question: "52. Word that is pronounced the same as another word but differs in meaning is called ______.",
+  A: "Homophone",
+  B: "Homograph",
+  C: "Diphthong",
+  D: "Syllable",
+  answer: "A"
+},
+{
+  question: "53. ______ can help me learn English.",
+  A: "Dedication",
+  B: "Fear",
+  C: "Anger",
+  D: "Shyness",
+  answer: "A"
+},
+{
+  question: "54. Which punctuation mark should be used in the sentence: 'Is there any site engineer here'",
+  A: "Question mark (?)",
+  B: "Comma (,)",
+  C: "Full stop (.)",
+  D: "Exclamation (!)",
+  answer: "A"
+},
+{
+  question: "55. When is capital letter used?",
+  A: "Beginning of a sentence",
+  B: "Names of persons",
+  C: "Names of places",
+  D: "All the above",
+  answer: "D"
+},
+{
+  question: "56. Which one of the following is a describing word?",
+  A: "Eat",
+  B: "Repair",
+  C: "Hot",
+  D: "Soup",
+  answer: "C"
+},
+{
+  question: "57. When someone says 'Thank you' we should reply and say ______.",
+  A: "Thank you",
+  B: "You're welcome",
+  C: "Bye-bye",
+  D: "How are you?",
+  answer: "B"
+},
+{
+  question: "58. Choose the odd word.",
+  A: "Please",
+  B: "Sorry",
+  C: "Paper",
+  D: "Thanks",
+  answer: "C"
+},
+{
+  question: "59. I am a welder. I work with ______.",
+  A: "Nail polish",
+  B: "Robots",
+  C: "Welding tools",
+  D: "Laptops",
+  answer: "C"
+},
+{
+  question: "60. Today's weather is going to be ______.",
+  A: "Hopeful",
+  B: "Warm",
+  C: "Blue",
+  D: "Urgent",
+  answer: "B"
+},
+{
+  question: "61. The word 'when' denotes ______.",
+  A: "Place",
+  B: "Thing",
+  C: "Person",
+  D: "Time",
+  answer: "D"
+},
+{
+  question: "62. Rahul enjoys ______ with his pet dog.",
+  A: "Playing",
+  B: "Writing",
+  C: "Doing",
+  D: "Washing",
+  answer: "A"
+},
+{
+  question: "63. Having a clear idea of our personality including strengths, weaknesses, thoughts, beliefs, motivation and values is called ______.",
+  A: "Belief",
+  B: "Self Awareness",
+  C: "Myth",
+  D: "Threat",
+  answer: "B"
+},
+{
+  question: "64. ______ are the abilities and qualities you have that help you grow in life and career.",
+  A: "Personal Strengths",
+  B: "Personal Weaknesses",
+  C: "Opportunities",
+  D: "Threats",
+  answer: "A"
+},
+{
+  question: "65. ______ are the qualities that are considered negative and need improvement.",
+  A: "Personal Strengths",
+  B: "Personal Weaknesses",
+  C: "Opportunities",
+  D: "Threats",
+  answer: "B"
+},
+{
+  question: "66. Which of the following is an example of personal weakness?",
+  A: "Confidence",
+  B: "Being lazy",
+  C: "Hard working",
+  D: "Active",
+  answer: "B"
+},
+{
+  question: "67. Goals are of two types.",
+  A: "Good and bad goals",
+  B: "Long-term and short-term goals",
+  C: "Rich and poor goals",
+  D: "Fixed-term and non-fixed term goals",
+  answer: "B"
+},
+{
+  question: "68. What is stress?",
+  A: "Feeling of joy",
+  B: "Feeling of surprise",
+  C: "Feeling of delight",
+  D: "Feeling of frustration/disappointment",
+  answer: "D"
+},
+{
+  question: "69. Which of these is a sign of good stress?",
+  A: "It prevents you from pursuing your goals",
+  B: "It makes you doubt yourself",
+  C: "Does not lead to long-term tension or loss of sleep",
+  D: "Makes you physically ill or causes pain",
+  answer: "C"
+},
+{
+  question: "70. Which of the following is NOT a stress buster?",
+  A: "Spending long hours thinking about the problem",
+  B: "Going to bed on time",
+  C: "Listening to music",
+  D: "Gardening",
+  answer: "A"
+},
+{
+  question: "71. What is mindfulness?",
+  A: "Technique of not minding anything",
+  B: "Sharpening your brain",
+  C: "Ability to be fully present in a moment",
+  D: "People full of thoughts",
+  answer: "C"
+},
+{
+  question: "72. Which of the following is true about stress?",
+  A: "Stress can be managed",
+  B: "Stress cannot be controlled",
+  C: "Stress cannot be decreased",
+  D: "All of the above",
+  answer: "A"
+},
+{
+  question: "73. Which of the following statements is true?",
+  A: "Only experts can solve problems",
+  B: "The more problems you solve, the better you become",
+  C: "Need a course to solve every problem",
+  D: "Solving problems is not important",
+  answer: "B"
+},
+{
+  question: "74. Which should be considered while making decisions?",
+  A: "Your mood",
+  B: "Friends' choices",
+  C: "Community rules",
+  D: "Your personal priorities",
+  answer: "D"
+},
+{
+  question: "75. What is the full form of SWOT?",
+  A: "Special Wellness Organization Theme",
+  B: "Speak Work Openly Together",
+  C: "Strong World Olympic Team",
+  D: "Strength Weakness Opportunity Threat",
+  answer: "D"
+}
