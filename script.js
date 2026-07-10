7d6fef999514f5f70904a25e96b5ed44db128bfa
Baban <voice9141@gmail.com>
2026-07-11 01:12:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -263,7 +263,8 @@ function startTimer() {
 function showAbout() {
     alert(
         "👨‍💻 Ravi Tech Academy\n\n" +
-        "Designed by R. Yadav\ 0x41✨\n\n" +
+        "Designed by R. Yadav\n\n" +
+        "Sp 0x41✨\n\n" +
         "Built for Electronics Mechanic Students."
     );
 }
